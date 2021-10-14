0cfab3a76c1f17a3a656c205e6585f42bd7d63bd
hoard worrier <me@opensource.com>
2021-10-14 07:57:58 -0400

    Fixed typo

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosInstance } from 'axios'
 import { IUPACResponse } from '../types'
 
-const Axios: AxiosInstance = axios.create({
+const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_API_URL,
 })
 
 const api = {
   getIUPACFromSMILES: (smiles: string) =>
-    Axios.get<IUPACResponse>('/iupac', { params: { smiles } }),
+    axiosInstance.get<IUPACResponse>('/iupac', { params: { smiles } }),
 }
 
 export default api
